a54941e94e8b5335e6245e840b1be8d822913f85
AderHenrique <adercontas@gmail.com>
2024-10-30 16:54:33 -0300

    Cria seção Hero do site

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Cart from "./Pages/Cart";
+import Shop from "./Pages/Shop";
+import Product from "./Pages/Product";
+import LoginSignup from "./Pages/LoginSignup";
+import ShopCategoty from "./Pages/ShopCategoty";
 function App() {
-  return <Navbar />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategoty category="mens" />} />
+        <Route path="/womens" element={<ShopCategoty category="womens" />} />
+        <Route path="/kids" element={<ShopCategoty category="kids" />} />
+        <Route path="/product" element={<Product />}>
+          <Route path=":product:Id" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
